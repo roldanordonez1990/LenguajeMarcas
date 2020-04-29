44ed9c3c4ae759e58abdddf431c79c1aaa613a6b
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-04-29 22:53:57 +0200

    no se mueve ni jarta de vino

--- src/JAVA SCRIPT/Página de Carga/JS/index.js
+++ src/JAVA SCRIPT/Página de Carga/JS/index.js
@@ -1,7 +1,14 @@
 var canvas,
         ctx,
-        imgPelota, // Imagen del background del juego
+        imgFondo, // Imagen del background del juego
+        imgPelota,
         imgCargadas = 0;
+
+        //Coordenadas iniciales
+        var CoorX = 340;
+        var CoorY = 200;
+        var arrastrar = false;
+        var lanzar = false;
 
 
         function init() {
@@ -13,30 +20,45 @@ var canvas,
             // Necesitamos el contexto del canvas, para poderlo utilizar como "brocha", gracias a este elemento podremos
             // asignar colores y pintar primitivas, imágenes, textos, etc.
             ctx = canvas.getContext("2d");
-    
+           
             // Inicialización de la palabra al azar, se obtiene un número al azar para determinar una palabra del array de palabras posibles
-             alert("Hola, Bienvenid@s");
+             //alert("Hola, Bienvenid@s");
             // El array de "coincidencias" tendrá tantos elementos como letras tenga la palabra al azar. Todos los elementos se inicializarán
             // al carácter '_', que indicará que esa letra aún no se ha descubierto en la palabra secreta
-            
+         tecla(e);
         }
 
 
         function preloadImages() {
             // Carga de la imagen del fondo del juego
-            imgPelota = new Image();
-            imgPelota.src = 'images/campo.jpg';
-            imgPelota.addEventListener('load', function() {
+            imgFondo = new Image();
+            imgFondo.src = 'images/campo.jpg';
+            imgPelota =document.getElementById("ima");
+            imgFondo.addEventListener('load', function() {
                 // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
                 imgCargadas++;
                 paintEscena();
               }, false);
-    
-          }  
+
+              // Cargamos la imagen de la pelota
+
+              imgPelota = new Image();
+              imgPelota.src = 'images/pelota.png';
+              imgPelota.id = "ima";
+              imgPelota.addEventListener('load', function() {
+                // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
+                imgCargadas++;
+
+                
+                
+                paintEscena();
+              }, false);
+
+     
 
           function paintEscena () {
             // Sólo pasamos a pintar la escena si nos aseguramos de que las dos imágenes han sido cargadas correctamente.
-            if (imgCargadas == 1) {
+            if (imgCargadas == 2) {
                 // Pintamos el fondo, el personaje, los caracteres adivinados y los fallos comentidos por el usuario. Cada cosa en su función
                 paintFondo();
             
@@ -45,7 +67,21 @@ var canvas,
 
         function paintFondo () {
             // Pinto el fondo de la escena
-            ctx.drawImage(imgPelota, 0, 0);
-    
+            ctx.drawImage(imgFondo, 0, 0);
+            ctx.drawImage(imgPelota, CoorX, CoorY);
+            
         }
-    
+
+      
+        document.addEventListener('keypress', onDocumentKeyDown, false);
+        function onDocumentKeyDown(event) {
+          var keyCode = event.which;
+          if (e.keyCode == 39) {
+              CoorX=CoorX+20;//mueve la nave 5 pixeles a la derecha
+      init();
+          }
+
+    }  
+       
+        
+}
