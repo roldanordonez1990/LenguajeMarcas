2c674ec2c0d52166291a60a3aa411c274f38ac3f
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-05-01 02:53:23 +0200

    NUEVO

--- src/JAVA SCRIPT/Página de Carga/JS/index.js
+++ src/JAVA SCRIPT/Página de Carga/JS/index.js
@@ -2,38 +2,52 @@ var canvas,
         ctx,
         imgFondo, // Imagen del background del juego
         imgPelota,
+        vx = 5,
+        gravity = 1,
+        WIDTH = 100,
+        HEIGHT = 100,
         imgCargadas = 0;
 
+        
         //Coordenadas iniciales
-        var CoorX = 340;
+        var CoorX = 350;
         var CoorY = 200;
-        var arrastrar = false;
-        var lanzar = false;
 
+        
 
         function init() {
-            // Lo primero es comenzar a cargar las imágenes
+            
             preloadImages();
-    
+           
             // Obtención del elemento html con id = "canvas". Puedes mirar el código html para entender mejor esto
             canvas = document.getElementById('canvas');
             // Necesitamos el contexto del canvas, para poderlo utilizar como "brocha", gracias a este elemento podremos
             // asignar colores y pintar primitivas, imágenes, textos, etc.
             ctx = canvas.getContext("2d");
+
+           // document.addEventListener("keydown", moverDerecha);
+
+           
+            
            
             // Inicialización de la palabra al azar, se obtiene un número al azar para determinar una palabra del array de palabras posibles
              //alert("Hola, Bienvenid@s");
             // El array de "coincidencias" tendrá tantos elementos como letras tenga la palabra al azar. Todos los elementos se inicializarán
             // al carácter '_', que indicará que esa letra aún no se ha descubierto en la palabra secreta
-         tecla(e);
+           
         }
 
 
         function preloadImages() {
+
+            
+            // Lo primero es comenzar a cargar las imágenes
+          
+            
             // Carga de la imagen del fondo del juego
             imgFondo = new Image();
             imgFondo.src = 'images/campo.jpg';
-            imgPelota =document.getElementById("ima");
+           
             imgFondo.addEventListener('load', function() {
                 // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
                 imgCargadas++;
@@ -44,17 +58,37 @@ var canvas,
 
               imgPelota = new Image();
               imgPelota.src = 'images/pelota.png';
-              imgPelota.id = "ima";
+             
               imgPelota.addEventListener('load', function() {
                 // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
                 imgCargadas++;
-
-                
-                
                 paintEscena();
+
               }, false);
 
-     
+              document.addEventListener("keydown", function(event){
+                  
+                if(event.keyCode == 39){
+                    console.log("Has pulsado la derecha");
+                    moverDerecha();
+                    paintEscena();
+                }
+                if(event.keyCode == 37){
+                 console.log("Has pulsado la izquierda");
+                 moverIzquierda();
+                 paintEscena();
+                }
+                if(event.keyCode == 38){
+                    console.log("Has pulsado la izquierda");
+                    moverArriba();
+                    paintEscena();
+                   }
+                   if(event.keyCode == 40){
+                    console.log("Has pulsado la izquierda");
+                    moverAbajo();
+                    paintEscena();
+                   }   
+             });
 
           function paintEscena () {
             // Sólo pasamos a pintar la escena si nos aseguramos de que las dos imágenes han sido cargadas correctamente.
@@ -68,20 +102,52 @@ var canvas,
         function paintFondo () {
             // Pinto el fondo de la escena
             ctx.drawImage(imgFondo, 0, 0);
-            ctx.drawImage(imgPelota, CoorX, CoorY);
+            ctx.drawImage(imgPelota, CoorX, CoorY, WIDTH, HEIGHT);
             
-        }
+        }    
 
-      
-        document.addEventListener('keypress', onDocumentKeyDown, false);
-        function onDocumentKeyDown(event) {
-          var keyCode = event.which;
-          if (e.keyCode == 39) {
-              CoorX=CoorX+20;//mueve la nave 5 pixeles a la derecha
-      init();
-          }
-
-    }  
-       
         
 }
+
+function moverDerecha(){
+    
+    //imgPelota = this.CoorX + 5;
+    CoorX = CoorX+28;
+
+    if(CoorX > (canvas.width - this.WIDTH )){
+        CoorX = canvas.width - this.WIDTH
+    }
+   
+  }
+
+  function moverIzquierda(){
+    
+    //imgPelota = this.CoorX + 5;
+    CoorX = CoorX-28;
+
+    if(CoorX < 0){
+        CoorX = 0;
+    }
+   
+  }
+
+  function moverArriba(){
+    
+    //imgPelota = this.CoorX + 5;
+    CoorY = CoorY-28;
+    if(CoorY < 0){
+        CoorY = 0;
+    }
+  }
+
+  function moverAbajo(){
+    
+    //imgPelota = this.CoorX + 5;
+    CoorY = CoorY+28;
+
+    if(CoorY > (canvas.height - this.HEIGHT )){
+        CoorY = canvas.height - this.HEIGHT
+    }
+
+   
+  }
