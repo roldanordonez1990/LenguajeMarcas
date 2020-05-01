7c2e07dff447b679036aede4b2513274321730fe
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-05-02 01:57:16 +0200

    Fin

--- src/JAVA SCRIPT/Página de Carga/JS/index.js
+++ src/JAVA SCRIPT/Página de Carga/JS/index.js
@@ -1,211 +1,231 @@
 var canvas,
-        ctx,
-        imgFondo, // Imagen del background del juego
-        imgPelota,
-        imgBomba,
-        
-        imgCargadas = 0;
+ctx,
+imgFondo, // Imagen del background del juego
+imgPelota,
+imgBomba,
+imgZapatilla,
+colapso = false;
 
-        
-        //Coordenadas para la pelota
-        var CoorX = 150;
-        var CoorY = 200;
-        var WIDTH = 100;
-        var HEIGHT = 100;
-
-        //Coordenadas para la bomba
-        var CoorBombaX = 0;
-        var CoorBombaY = 250;
-        var BombaWidth = 100;
-        var BombaHeight = 100;
-        var bx = 50;
-        var by = -20;
-        
-        
+imgCargadas = 0;
 
 
-        
+//Coordenadas para la pelota
+var CoorX = 360;
+var CoorY = 220;
+var WIDTH = 80;
+var HEIGHT = 80;
 
-        function init() {
+//Coordenadas para la bomba
+var CoorBombaX = 0;
+var CoorBombaY = 250;
+var BombaWidth = 80;
+var BombaHeight = 80;
+var bx = 5;
+var by = -5;
+
+//Coordenadas para la zapatilla
+var CoorZapaX = 700;
+var CoorZapaY = 100;
+var ZapaWidth = 80;
+var ZapaHeight = 80;
+var zx = 5;
+var zy = -5;
+
+
+
+function init() {
+  alert("Intenta no chocar con los objetos moviéndote con la pelota");
+    preloadImages();
+   
+    // Obtención del elemento html con id = "canvas". Puedes mirar el código html para entender mejor esto
+    canvas = document.getElementById('canvas');
+    // Necesitamos el contexto del canvas, para poderlo utilizar como "brocha", gracias a este elemento podremos
+    // asignar colores y pintar primitivas, imágenes, textos, etc.
+    ctx = canvas.getContext("2d");
+
+    // Start the first frame request
+    window.requestAnimationFrame(gameLoop);
+
+   
+  
+}
+
+function gameLoop(timeStamp){
+  paintEscena();
+
+  // Keep requesting new frames
+  window.requestAnimationFrame(gameLoop);
+}
+
+
+
+function preloadImages() {
+
+    
+    // Lo primero es comenzar a cargar las imágenes
+  
+    
+    // Carga de la imagen del fondo del juego
+    imgFondo = new Image();
+    imgFondo.src = 'images/campo.jpg';
+   
+    imgFondo.addEventListener('load', function() {
+        // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
+        imgCargadas++;
+        paintEscena();
+      }, false);
+
+      // Carga de la imagen del fondo del juego
+    imgZapatilla = new Image();
+    imgZapatilla.src = 'images/zapatilla.png';
+   
+    imgFondo.addEventListener('load', function() {
+        // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
+        imgCargadas++;
+        paintEscena();
+      }, false);
+
+
+       // Carga de la imagen de la bomba del juego
+    imgBomba = new Image();
+    imgBomba.src = 'images/bomba.png';
+   
+    imgBomba.addEventListener('load', function() {
+        // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
+        
+        imgCargadas++;
+        paintEscena();
+      }, false);
+      
+      document.addEventListener("keydown", function(event){
           
-            preloadImages();
-           
-            // Obtención del elemento html con id = "canvas". Puedes mirar el código html para entender mejor esto
-            canvas = document.getElementById('canvas');
-            // Necesitamos el contexto del canvas, para poderlo utilizar como "brocha", gracias a este elemento podremos
-            // asignar colores y pintar primitivas, imágenes, textos, etc.
-            ctx = canvas.getContext("2d");
-
-              // Start the first frame request
-         window.requestAnimationFrame(gameLoop);
-
-           // document.addEventListener("keydown", moverDerecha);
-
-           
-           
-            // Inicialización de la palabra al azar, se obtiene un número al azar para determinar una palabra del array de palabras posibles
-             //alert("Hola, Bienvenid@s");
-            // El array de "coincidencias" tendrá tantos elementos como letras tenga la palabra al azar. Todos los elementos se inicializarán
-            // al carácter '_', que indicará que esa letra aún no se ha descubierto en la palabra secreta
-           
-          
+        if(event.keyCode == 39){
+            console.log("Has pulsado la derecha");
+            moverDerecha();
+            paintEscena();
         }
+        if(event.keyCode == 37){
+         console.log("Has pulsado la izquierda");
+         moverIzquierda();
+         paintEscena();
+        }
+        if(event.keyCode == 38){
+            console.log("Has pulsado la izquierda");
+            moverArriba();
+            paintEscena();
+           }
+           if(event.keyCode == 40){
+            console.log("Has pulsado la izquierda");
+            moverAbajo();
+            paintEscena();
+           }   
+     });
 
-        
+     // Cargamos la imagen de la pelota
 
-        function preloadImages() {
+     imgPelota = new Image();
+     imgPelota.src = 'images/pelota.png';
+    
+     imgPelota.addEventListener('load', function() {
+       // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
+       imgCargadas++;
+       paintEscena();
 
-         
-            // Lo primero es comenzar a cargar las imágenes
-          
-            
-            // Carga de la imagen del fondo del juego
-            imgFondo = new Image();
-            imgFondo.src = 'images/campo.jpg';
-           
-            imgFondo.addEventListener('load', function() {
-                // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
-                imgCargadas++;
-                paintEscena();
-              }, false);
+     }, false); 
+}
+
+function paintEscena () {
+// Sólo pasamos a pintar la escena si nos aseguramos de que las dos imágenes han sido cargadas correctamente.
+if (imgCargadas == 4) {
+refrescarMundo();
+refrescaZapatilla();
+// Pintamos el fondo, el personaje, los caracteres adivinados y los fallos comentidos por el usuario. Cada cosa en su función
+paintFondo();
+}
+}
+
+function paintFondo () {
+// Pinto el fondo de la escena
+ctx.drawImage(imgFondo, 0, 0);
+ctx.drawImage(imgPelota, CoorX, CoorY, WIDTH, HEIGHT);
+ctx.drawImage(imgBomba, CoorBombaX, CoorBombaY, BombaWidth, BombaHeight);
+ctx.drawImage(imgZapatilla, CoorZapaX, CoorZapaY, ZapaWidth, ZapaHeight);
 
 
-              window.onload= function(){
-                
-              }
-               // Carga de la imagen de la bomba del juego
-            imgBomba = new Image();
-            imgBomba.src = 'images/bomba.png';
-           
-            imgBomba.addEventListener('load', function() {
-                // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
-                
-                imgCargadas++;
-                paintEscena();
-              }, false);
-              
-              
-       
-             
-              
-
-              document.addEventListener("keydown", function(event){
-                  
-                if(event.keyCode == 39){
-                    console.log("Has pulsado la derecha");
-                    moverDerecha();
-                    paintEscena();
-                }
-                if(event.keyCode == 37){
-                 console.log("Has pulsado la izquierda");
-                 moverIzquierda();
-                 paintEscena();
-                }
-                if(event.keyCode == 38){
-                    console.log("Has pulsado la izquierda");
-                    moverArriba();
-                    paintEscena();
-                   }
-                   if(event.keyCode == 40){
-                    console.log("Has pulsado la izquierda");
-                    moverAbajo();
-                    paintEscena();
-                   }   
-             });
-
-             // Cargamos la imagen de la pelota
-
-             imgPelota = new Image();
-             imgPelota.src = 'images/pelota.png';
-            
-             imgPelota.addEventListener('load', function() {
-               // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
-               imgCargadas++;
-               paintEscena();
-
-             }, false);
-
-          function paintEscena () {
-            // Sólo pasamos a pintar la escena si nos aseguramos de que las dos imágenes han sido cargadas correctamente.
-            if (imgCargadas == 3) {
-              refrescarMundo();
-                // Pintamos el fondo, el personaje, los caracteres adivinados y los fallos comentidos por el usuario. Cada cosa en su función
-                paintFondo();
-                
-            
-            }
-        } 
-
-        function paintFondo () {
-            // Pinto el fondo de la escena
-            ctx.drawImage(imgFondo, 0, 0);
-            ctx.drawImage(imgPelota, CoorX, CoorY, WIDTH, HEIGHT);
-            ctx.drawImage(imgBomba, CoorBombaX, CoorBombaY, BombaWidth, BombaHeight);
-            
-            
-        }    
 }
 
 
 function moverDerecha(){
-    
-    //imgPelota = this.CoorX + 5;
-    CoorX = CoorX+35;
 
-    if(CoorX > (canvas.width - this.WIDTH )){
-        CoorX = canvas.width - this.WIDTH
-    }
-   
-  }
+//imgPelota = this.CoorX + 5;
+CoorX = CoorX+50;
 
-  function moverIzquierda(){
-    
-    //imgPelota = this.CoorX + 5;
-    CoorX = CoorX-35;
-
-    if(CoorX < 0){
-        CoorX = 0;
-    }
-   
-  }
-
-  function moverArriba(){
-    
-    //imgPelota = this.CoorX + 5;
-    CoorY = CoorY-35;
-    if(CoorY < 0){
-        CoorY = 0;
-    }
-  }
-
-  function moverAbajo(){
-    
-    //imgPelota = this.CoorX + 5;
-    CoorY = CoorY+35;
-
-    if(CoorY > (canvas.height - this.HEIGHT )){
-        CoorY = canvas.height - this.HEIGHT
-    }
-
-   
-  }
-
-
-  function refrescarMundo() {
-    //Vamos acercando al borde izquierdo los obstaculos
-    CoorBombaX += bx;
+if(CoorX > (canvas.width - this.WIDTH )){
+CoorX = canvas.width - this.WIDTH
+}
 
 }
 
-  function gameLoop(timeStamp){
-    paintEscena();
+function moverIzquierda(){
 
-    // Keep requesting new frames
-    window.requestAnimationFrame(gameLoop);
+//imgPelota = this.CoorX + 5;
+CoorX = CoorX-50;
+
+if(CoorX < 0){
+CoorX = 0;
 }
-   
+
+}
+
+function moverArriba(){
+
+//imgPelota = this.CoorX + 5;
+CoorY = CoorY-50;
+if(CoorY < 0){
+CoorY = 0;
+}
+}
+
+function moverAbajo(){
+
+//imgPelota = this.CoorX + 5;
+CoorY = CoorY+50;
+
+if(CoorY > (canvas.height - this.HEIGHT )){
+CoorY = canvas.height - this.HEIGHT
+}
+
+
+}
+
+function refrescarMundo() {
+//Vamos acercando al borde izquierdo los obstaculos
+CoorBombaX += bx;
+CoorBombaY += by;
+if(CoorBombaX + bx > canvas.width -60|| CoorBombaX + bx < 0) {
+  bx = -bx;
+}
+if(CoorBombaY + by > canvas.height -100|| CoorBombaY + by < 0) {
+  by = -by;
+}
+
+}
+
+function refrescaZapatilla() {
+  //Vamos acercando al borde izquierdo los obstaculos
+  CoorZapaX += zx;
+  CoorZapaY += zy;
+  if(CoorZapaX + bx > canvas.width -60|| CoorZapaX + zx < 0) {
+    zx = -zx;
+  }
+  if(CoorZapaY + zy > canvas.height -100|| CoorZapaY + zy < 0) {
+    zy = -zy;
+  }
+
   
+  }
   
+
 
    
 
