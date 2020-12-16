98f41dae3b9cc82121b182275ba542835848360e
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-12-16 13:05:04 +0100

    juego js

--- src/JAVA SCRIPT/Página de Carga/JS/index.js
+++ src/JAVA SCRIPT/Página de Carga/JS/index.js
@@ -3,7 +3,7 @@
 var canvas,
 ctx,
 imgFondo,
-imgPelota,
+imgCabeza,
 imgBomba,
 imgZapatilla,
 imgGameOver,
@@ -11,45 +11,43 @@ finDeJuego = false,
 imgVida,
 imgCargadas = 0;
 
-//Coordenadas y dimensiones para la pelota
+//Coordenadas y dimensiones para la cabeza
 var CoorX = 250;
 var CoorY = 220;
-var WIDTH = 80;
-var HEIGHT = 80;
-var px = 50;
-var py = 50;
+var WIDTH = 70;
+var HEIGHT = 60;
+var px = 70;
+var py = 70;
 
-//Coordenadas y dimensiones para la bomba
+//Coordenadas y dimensiones para la nieve
 var CoorBombaX = 0;
 var CoorBombaY = 250;
-var BombaWidth = 80;
-var BombaHeight = 80;
-var bx = 5;
-var by = -5;
+var BombaWidth = 70;
+var BombaHeight = 70;
+var bx = 6;
+var by = -6;
 
-//Coordenadas y dimensiones para la zapatilla
+//Coordenadas y dimensiones para el rayo
 var CoorZapaX = 700;
 var CoorZapaY = 100;
-var ZapaWidth = 80;
-var ZapaHeight = 80;
-var zx = 5;
-var zy = -5;
+var ZapaWidth = 90;
+var ZapaHeight = 60;
+var zx = 6;
+var zy = -6;
 
-//Array de vidas
-var vidas = new Array(imgVida, imgVida, imgVida);
-var vidasX = 100;
-var vidasY = 100;
-var vidasWidth = 80;
-var vidasHeight = 80;
-
-
-
-
-
+//Coordenadas y dimensiones de la vida
+var contadorVidas = 3;
+var CoorVidaX = 0;
+var CoorVidaY = 460;
+var VidaWidth = 90;
+var VidaHeight = 90;
 
 function init() {
-    alert("Intenta no chocar con los objetos moviéndote con la pelota");
+    //Cargamos los sonidos al iniciar el juego
+    var sonidoPerder = new Audio();
+    sonidoPerder.src='music/romper.mp3';
     preloadImages();
+    
     // Obtención del elemento html con id = "canvas". Puedes mirar el código html para entender mejor esto
     canvas = document.getElementById('canvas');
     // Necesitamos el contexto del canvas, para poderlo utilizar como "brocha", gracias a este elemento podremos
@@ -68,40 +66,40 @@ function init() {
     window.requestAnimationFrame(gameLoop);
   }
   
+
       //En esta función vamos a cargar todas las imágenes
       function preloadImages() {
     
         //Carga de la imagen del fondo del juego
         imgFondo = new Image();
-        imgFondo.src = 'images/campo.jpg';
+        imgFondo.src = 'images/cielo.png';
         imgFondo.addEventListener('load', function() {
         // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
         imgCargadas++;
         paintEscena();
       }, false);
 
-       //Carga de la imagen del fondo del juego
-       imgVida = new Image();
-       imgVida.src = 'images/vida.png';
-       imgVida.addEventListener('load', function() {
+      // Carga de la imagen de la zapatilla del juego
+        imgVida = new Image();
+        imgVida.src = 'images/vida.png';
+        imgFondo.addEventListener('load', function() {
+        // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
+        imgCargadas++;
+        paintEscena();
+      }, false);
+
+       // Carga de la imagen de la zapatilla del juego
+       imgZapatilla = new Image();
+       imgZapatilla.src = 'images/rayo.png';
+       imgFondo.addEventListener('load', function() {
        // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
        imgCargadas++;
        paintEscena();
      }, false);
 
-      // Carga de la imagen de la zapatilla del juego
-        imgZapatilla = new Image();
-        imgZapatilla.src = 'images/zapatilla.png';
-        imgFondo.addEventListener('load', function() {
-        // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
-        imgCargadas++;
-        paintEscena();
-      }, false);
-
-
        // Carga de la imagen de la bomba del juego
         imgBomba = new Image();
-        imgBomba.src = 'images/bomba.png';
+        imgBomba.src = 'images/copo.png';
         imgBomba.addEventListener('load', function() {
         // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
         imgCargadas++;
@@ -138,9 +136,9 @@ function init() {
      });
      
         // Carga de la imagen de la pelota del juego
-        imgPelota = new Image();
-        imgPelota.src = 'images/pelota.png';
-        imgPelota.addEventListener('load', function() {
+        imgCabeza = new Image();
+        imgCabeza.src = 'images/cabeza.png';
+        imgCabeza.addEventListener('load', function() {
         // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
         imgCargadas++;
         paintEscena();
@@ -153,17 +151,17 @@ function init() {
         // Este trozo de código se ejecutará de manera asíncrona cuando la imagen se haya realmente cargado.
         }, false);
 }
-
 function paintEscena () {
   //Las imágenes aparecerán en escena si el total de imágenes es el indicado
-  if (imgCargadas = 5) {
+  if (imgCargadas = 6) {
         //pintamos las nuevas posiciones de los objetos y se irán pintando en cada iteración del frame que es llamado en gameLoop
         moverBomba();
         moverZapatilla();
         colision();
         paintFondo();
+
   //Le damos la condición de que si es finDeJuego, pinte en escena el game over
-  if (finDeJuego){
+  if (contadorVidas == 0){
       gameOver();
     }
   }
@@ -172,15 +170,14 @@ function paintEscena () {
 function paintFondo () {
 // Pintamos lo que va a aparecer en escena al empezar el juego
   ctx.drawImage(imgFondo, 0, 0);
-  ctx.drawImage(imgPelota, CoorX, CoorY, WIDTH, HEIGHT);
+  ctx.drawImage(imgCabeza, CoorX, CoorY, WIDTH, HEIGHT);
   ctx.drawImage(imgBomba, CoorBombaX, CoorBombaY, BombaWidth, BombaHeight);
   ctx.drawImage(imgZapatilla, CoorZapaX, CoorZapaY, ZapaWidth, ZapaHeight);
-  for(var i = 0; i <vidas.length; i++){
-    vidas [i] = ctx.drawImage(imgVida, vidasX, vidasY, vidasWidth, vidasHeight);
+  for(var i = 0; i < contadorVidas; i++){
+    ctx.drawImage(imgVida, CoorVidaX + 65*i, CoorVidaY, VidaWidth, VidaHeight);
+    }
   }
   
-}
-
 //Movemos hacia la derecha la pelota sumando coordenadas
 function moverDerecha (){
   CoorX += px;
@@ -221,7 +218,7 @@ function moverBomba() {
   //Le damos una velocidad fija a la bomba y le decimos que no salga del canvas
   CoorBombaX += bx;
   CoorBombaY += by;
-  if(CoorBombaX + bx > canvas.width -60 || CoorBombaX + bx < 0) {
+  if(CoorBombaX + bx > canvas.width - 90 || CoorBombaX + bx < 0) {
   //Cuando vaya a salir del canvas, le cambiamos de signo para que rebote
     bx = -bx;
   }
@@ -234,7 +231,7 @@ function moverBomba() {
     //Le damos una velocidad fija a la zapatilla y le decimos que no salga del canvas
     CoorZapaX += zx;
     CoorZapaY += zy;
-    if(CoorZapaX + zx > canvas.width -60 || CoorZapaX + zx < 0) {
+    if(CoorZapaX + zx > canvas.width -90 || CoorZapaX + zx < 0) {
     //Cuando vaya a salir del canvas, le cambiamos de signo para que rebote
       zx = -zx;
     }
@@ -247,44 +244,95 @@ function moverBomba() {
   function colision(){
   //Colisión para la imagen de la zapatilla
   if (CoorX < CoorZapaX + ZapaWidth &&
-    CoorX + WIDTH > CoorZapaX  &&
-    CoorY < CoorZapaY + ZapaHeight &&
-    HEIGHT + CoorY > CoorZapaY) {
-      //Cuando detecte una colisión, ponemos la bandera a true
-      finDeJuego = true;
+      CoorX + WIDTH > CoorZapaX  &&
+      CoorY < CoorZapaY + ZapaHeight &&
+      HEIGHT + CoorY > CoorZapaY) {
+
+      var sonidoPerder = new Audio();
+      sonidoPerder.src='music/romper.mp3';
+      sonidoPerder.play();
       
-     //alert("¡GAME OVER! ¡HAS CHOCADO! Pulsa para otra partida");
-    //document.location.reload();
-      //¡colision detectada!
-     console.log("Colision");
+      if ((CoorZapaX <= CoorX+WIDTH && CoorZapaX >= CoorX) || (CoorZapaX+ZapaWidth >= CoorX && CoorZapaX+ZapaWidth <= CoorX+WIDTH ))
+        zx = -zx;
+      if ((CoorZapaY <= CoorY+HEIGHT && CoorZapaY >= CoorY) || (CoorZapaY+ZapaHeight >= CoorY && CoorZapaY+ZapaHeight <= CoorY+HEIGHT ))
+        zy = -zy;
+
+        
+      contadorVidas--;
+
+      console.log("Colision");
  }
  //Colisión para la imagen de la bomba
  if(CoorX < CoorBombaX  + BombaWidth &&
   CoorX + WIDTH  > CoorBombaX  &&
   CoorY < CoorBombaY + BombaHeight &&
   HEIGHT + CoorY > CoorBombaY){
-    //Cuando detecte una colisión, ponemos la bandera a true
-    finDeJuego = true;
-  
-   //alert("¡GAME OVER! ¡HAS CHOCADO! Pulsa para otra partida");
- 
-    //document.location.reload();
-    //¡colision detectada!
-    console.log("Colision");
+    var sonidoPerder = new Audio();
+    sonidoPerder.src='music/romper.mp3';
+    sonidoPerder.play();
+    if ((CoorBombaX <= CoorX+WIDTH && CoorBombaX >= CoorX) || (CoorBombaX+BombaWidth >= CoorX && CoorBombaX+BombaWidth <= CoorX+WIDTH ))
+    bx = -bx;
+  if ((CoorBombaY <= CoorY+HEIGHT && CoorBombaY >= CoorY) || (CoorBombaY+BombaHeight >= CoorY && CoorBombaY+BombaHeight <= CoorY+HEIGHT ))
+    by = -by;
+    
+  contadorVidas--;
+  console.log("Colision");
  }
 }
 
+
 //Método el cual pintaremos el game over al finalizar el juego
 function gameOver () {
-  if(!finDeJuego){
-    window.requestAnimationFrame(gameLoop);
-  }
-  else{
+    
+  if(contadorVidas == 0){
+    setTimeout(function(){ 
+      bx = 0;
+      by = 0;
+      zy = 0;
+      zx = 0; 
+    }, 500);
     ctx.drawImage(imgGameOver,0,0);
+    //location.reload();
+    //window.requestAnimationFrame(gameLoop);
   }
+  
 }
 
 
+snowCount = 0;
+function snowFlakes(){
+    console.log(snowCount);
+    if(snowCount > 800){
+        
+        return false
+    }else{
+    var randomTime = Math.floor(Math.random() * (500) * 2);
+    setTimeout(function(){
+        snowCount = snowCount +1;
+        jquerysnow();
+       snowFlakes();
+    },randomTime);
+    }
+}
+function jquerysnow() {
 
- 
+       var snow = $('<div class="snow"></div>');
+        $('#snowflakes').prepend(snow);
+        snowX = Math.floor(Math.random() * $('#snowflakes').width());
+        snowSpd = Math.floor(Math.random() * (500) * 20);
 
+        snow.css({'left':snowX+'px'});
+    snow.html('<img src="images/gota.png" style="width: 50px;"/>');
+        snow.animate({
+            top: "500px",
+            opacity : "0",
+            
+
+        }, 2000, function(){
+            $(this).remove();
+            //jquerysnow();
+        });
+
+}
+
+snowFlakes()
